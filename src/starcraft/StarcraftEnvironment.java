d39d8916c850ec432917ec8073f44194f8530e43
Alberto <Alberto@Alberto-PC>
2015-04-10 09:41:10 +0200

    Este es el environment que tengo yo, basicamente tiene lo de si se va acercando y la distancia euclidiana

--- src/starcraft/StarcraftEnvironment.java
+++ src/starcraft/StarcraftEnvironment.java
@@ -1,31 +1,33 @@
 package starcraft;
 
-import entrada_salida.Log;
+import java.util.ArrayList;
+
 import q_learning.Action;
 import q_learning.Environment;
 import q_learning.State;
-import q_learning.VisitedStateTable;
 import bwapi.Game;
-import bwapi.Position;
 import bwapi.Unit;
 
 public class StarcraftEnvironment implements Environment{
-
-	private static int BOX_LENGTH = 32;
 	
 	private Game game;
 	private Unit unit;
-	private State state;
-	private State finalState;
-	private VisitedStateTable vTable;
+	private State previousState;
+	private Action previousAction;
+	//we don not need to save the current state, because we get it in runtime
+	private ArrayList<Integer> finalState; //array with the hash values of all possible final states 
+	//private VisitedStateTable vTable;
 	
-	public StarcraftEnvironment(Game game, Unit unit, State finalState) {
+	public StarcraftEnvironment(Game game, Unit unit, ArrayList<State> finalStates) {
 		this.game = game;
 		this.unit = unit;
-		this.state = new StarcraftState((int)unit.getPosition().getX()/BOX_LENGTH, 
-				(int)unit.getPosition().getY()/BOX_LENGTH, game.mapWidth(), game.mapHeight());
-		this.finalState = finalState;
-		vTable = new VisitedStateTable(numStates());
+		this.previousState = null;
+		this.previousAction = null;
+		this.finalState = new ArrayList<Integer>();
+		for(State sta : finalStates) {
+			finalState.add(sta.getValue());
+		}		
+		//vTable = new VisitedStateTable(numStates());
 	}
 	
 	@Override
@@ -35,150 +37,116 @@ public class StarcraftEnvironment implements Environment{
 
 	@Override
 	public int numActions() {
-		return StarcraftAction.values().length;
-	}
-	
-	public void setUnit(Unit u){
-		unit = u;
+		return Presenter.getInstance().getStarcraftActionManager().getNumActions();
 	}
 
 	@Override
-	public double execute(Action action) {
-		
-		double reward = 0;
-		 
-		// Current position
-		int posX = (int)unit.getPosition().getX()/BOX_LENGTH;
-		int posY = (int)unit.getPosition().getY()/BOX_LENGTH;
-		vTable.set(this.state.getValue(),true);
-		String action_str = ""; //aux variable to print the action taken
-		
-		StarcraftAction sc_action = (StarcraftAction)action;
-		switch(sc_action) {
-		 case MOVE_UP: 
-			 posY--;
-			 action_str = "ARRIBA";
-		     break;
-		 case MOVE_RIGHT: 
-			 posX++;
-			 action_str = "DERECHA";
-		     break;
-		 case MOVE_DOWN:
-			 posY++;
-			 action_str = "ABAJO";
-		     break;
-		 case MOVE_LEFT:
-			 posX--;
-			 action_str = "IZQUIERDA";
-		     break;
-		     
-		 case MOVE_UP_LEFT: 
-			 posY--;
-			 posX--;
-			 action_str = "ARRIBA-IZQUIERDA";
-		     break;
-		 case MOVE_UP_RIGHT: 
-			 posX++;
-			 posY--;
-			 action_str = "ARRIBA-DERECHA";
-		     break;
-		 case MOVE_DOWN_LEFT:
-			 posY++;
-			 posX--;
-			 action_str = "ABAJO-IZQUIERDA";
-		     break;
-		 case MOVE_DOWN_RIGHT:
-			 posX++;
-			 posY++;
-			 action_str = "ABAJO-DERECHA";
-		     break;
-		         
-		     
-		 default: 
-			 
-			 break;
-		}
-		
-		// Here we move the units or execute the actions.	 
-		// Later is a "switch" evaluating if the new State/Action would have a good/bad Reward
-		// Here you must enter all the rewards of learning
-		Position p = isValid(posX, posY);
-		
-		if (p != null) {
-			unit.move(p);
-			
-			state = new StarcraftState(posX, posY, game.mapWidth(), game.mapHeight());
-			if(isFinalState()) {
-				reward = 1000;
-			}
-			if(vTable.get(state.getValue())){
-			//	reward = -1;
-			}
-		} else {
-			reward = -100;
-		}
+	public void execute(Action action) {
 
-		return reward;
+		// Update the previous state and action before modifying the current state
+		this.previousState = state();
+		this.previousAction = action;
+		
+		action.configureContext();
+		action.execute();
 	}
 
 	@Override
 	public State state() {
-		return state;
+		return new StarcraftState((int)unit.getPosition().getX()/Presenter.getInstance().getBoxSize(), 
+				(int)unit.getPosition().getY()/Presenter.getInstance().getBoxSize(), game.mapWidth(), game.mapHeight());
+	}
+
+	@Override
+	public State previousState() {
+		return previousState;
 	}
 	
 	@Override
-	public State finalState() {
-		return finalState;
+	public Action previousAction() {
+		return previousAction;
 	}
-
+	
 	@Override
 	public boolean isFinalState() {
-		return state.getValue() == finalState.getValue();
+		return hasWon() || hasLost();
+	}
+	
+	private boolean hasWon() {
+		return finalState.contains(state().getValue());
+	}
+	
+	private boolean hasLost() {
+		return !unit.exists();
 	}
 
 	@Override
-	public boolean stateHasChanged() {
-		return !unit.isMoving();
+	public boolean stateHasChanged() {		
+		return !unit.isMoving();	
+	}
+	
+	@Override
+	public double getReward(State state) {
+		double reward = 0; //this value could be 0.001 or very small values
+		
+		//If the current distance to the final is bigger than the future increase the reward
+		if(getCloser(state.getValue()))
+			reward = 10;
+		
+		//System.out.println(reward);
+		// Here you must enter all the rewards of learning
+		if(hasWon()) { //if the unit reaches the goal
+			reward = 1000;
+		} else if(hasLost()) { //if the unit doesn't exist (lost game)
+			reward = -1;
+		} else if(previousState() != null && previousState().getValue() == state().getValue()) { //the prev. state is the same, then the action taken doesnt changed the state (not a valid movement)
+			reward = -10;
+//		} else if(vTable.get(state().getValue())) { //anti-loops: the unit is in a visited state
+//			reward = 0;
+		}
+
+		return reward;
 	}
 	
 	@Override
 	public void reset() {
-		vTable.clear();
-		// TODO Auto-generated method stub
+		this.previousState = null;
+		this.previousAction = null;
+		game.pauseGame();
+		//vTable.clear();
 		game.restartGame();
 	}
 	
-	/**
-	 * See if the converted to Starcraft logical position x, y is a valid position and can go up there
-	 * @param x Height
-	 * @param y Width
-	 * @return Starcraft position or null if there is not valid
-	 */
-	private Position isValid(int x, int y) {
-		Position p = new Position(x*BOX_LENGTH+(BOX_LENGTH/2), y*BOX_LENGTH+(BOX_LENGTH/2));
-		if((0 <= x) && (x < game.mapWidth()*BOX_LENGTH) && (0 <= y) && (y < game.mapHeight()*BOX_LENGTH)
-				&& game.hasPath(unit.getPosition(), p)){
-			boolean dontCol = true;
-			int i = 0;
-			Unit m;
+	private boolean getCloser(int newState){
+		boolean reachingEnd = false;
 			
-			// Check if collide with other units
-			while(dontCol && i < game.getAllUnits().size()){
-				m = game.getAllUnits().get(i);
-				if(itsInside(m.getTop(),m.getBottom(),m.getRight(),m.getLeft(), x*BOX_LENGTH+(BOX_LENGTH/2), y*BOX_LENGTH+(BOX_LENGTH/2))) {
-					return null;
-				}
-				i++;
-			}
+		if(previousState != null){
+			double currentDist = euclideanDist(previousState().getValue());
+			double futureDist = euclideanDist(newState);
 			
-			return p;
-		}else{
-			return null;
+			if(currentDist>futureDist) 
+				reachingEnd = true;
 		}
+		
+		return reachingEnd;
 	}
-
-	private boolean itsInside(int top, int bottom, int right, int left, int x, int y) {		
-		return (left<=x) && (x<=right) && (bottom>=y) && (y>=top);	
-	}
+	
+	private double euclideanDist(int newState){
+		double dist = Double.MAX_VALUE;
+		int actualY = (int)(newState /  game.mapHeight());
+		int actualX = newState % game.mapWidth();
+		
+		for(int i = 0; i < finalState.size(); i++){					
+			int futureY = (int)(finalState.get(i) /  game.mapHeight());
+			int futureX = finalState.get(i) % game.mapWidth();
+			int x1 = Math.abs(actualY -  futureY);
+			int x2 = Math.abs(actualX -  futureX);
+			double x = Math.sqrt((Math.pow(x1, 2) + Math.pow(x2, 2)));
+			if(x<dist){
+				dist = x;
+			}
+		}
+		return dist;
+    }
 
 }
