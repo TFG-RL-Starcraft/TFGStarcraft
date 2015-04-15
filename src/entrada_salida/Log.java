2d5c87a753f0f5fdaf76a579d2e064bc1bed3eee
Lin M. Dotor <linmdotor@gmail.com>
2015-04-15 15:55:01 +0200

    Modificado el log añadiendo la operación readLog

--- src/entrada_salida/Log.java
+++ src/entrada_salida/Log.java
@@ -1,8 +1,16 @@
 package entrada_salida;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import q_learning.QTable;
+import q_learning.QTable_Array;
+import starcraft.Presenter;
+import starcraft.StarcraftState;
 
 public class Log {
 	
@@ -39,5 +47,43 @@ public class Log {
 	           
 	        }
         }
+	}
+	
+	/*
+	 * Devuelve un ArrayList de String, con un String por línea.
+	 */
+	public ArrayList<String> readLog(String path)
+	{
+        FileReader fr = null;		
+		ArrayList<String> log = new ArrayList<String>();
+		
+		if (path != null) {
+			try {
+	         // Open the file in a BufferedReader for easier reading
+			 fr = new FileReader(path);
+			 BufferedReader br = new BufferedReader(fr);	         
+	         
+	         String line;
+	         while((line=br.readLine()) != null)
+	         {
+	        	 log.add(line);
+	         }
+	         
+	         
+	      } catch(Exception e) {
+	         e.printStackTrace();
+	      } finally {
+	         // Make sure that closes the file
+	         try {                    
+	            if(fr != null) {   
+	               fr.close();     
+	            }                  
+	         } catch(Exception e2) { 
+	            e2.printStackTrace();
+	         }
+	      }
+		}
+		
+		return log;
 	}
 }
